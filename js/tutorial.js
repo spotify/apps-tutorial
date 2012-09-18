4f46ce2bf19e8c11fa216fb9ccbc3d86016de0e5
mager <andrew.mager@gmail.com>
2012-09-18 14:50:29 -0400

    Added Drag & Drop tutorial

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,5 +1,11 @@
-/* Code by Andrew Mager */
+/* Code by Andrew Mager
+*
+*  Spotify green: #74c043
+*
+**/
+
 window.onload = function() {
+
     sp = getSpotifyApi(1);
     var models = sp.require('sp://import/scripts/api/models');
     var views = sp.require('sp://import/scripts/api/views');
@@ -25,5 +31,36 @@ window.onload = function() {
         }
         current.style.display = 'block';
     }
+
+
+    // Handle drops
+    var drop_box = document.querySelector('#drop_box');
+
+    drop_box.addEventListener('dragstart', function(e){
+        e.dataTransfer.setData('text/html', this.innerHTML);
+        e.dataTransfer.effectAllowed = 'copy';
+    }, false);
+
+    drop_box.addEventListener('dragenter', function(e){
+        if (e.preventDefault) e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        this.classList.add('over');
+    }, false);
+
+    drop_box.addEventListener('dragover', function(e){
+        if (e.preventDefault) e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        return false;
+    }, false);
+
+    drop_box.addEventListener('drop', function(e){
+        if (e.preventDefault) e.preventDefault();
+        var drop = models.Playlist.fromURI(e.dataTransfer.getData('text'));
+        console.log(drop);
+        this.classList.remove('over');
+        var success_message = document.createElement('p');
+        success_message.innerHTML = 'Playlist successfully dropped: ' + drop.uri;
+        this.appendChild(success_message);
+    }, false);
 
 }
