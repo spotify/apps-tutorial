bf6b5bafca6ccc61dbb866198faf32fb5bbec991
mager <andrew.mager@gmail.com>
2012-09-20 14:17:41 -0400

    Updated multiple tracks player tutorial

--- js/tutorial.js
+++ js/tutorial.js
@@ -68,13 +68,31 @@ window.onload = function() {
     var single_track = models.Track.fromURI('spotify:track:0blzOIMnSXUKDsVSHpZtWL');
     var single_track_playlist = new models.Playlist();
     single_track_playlist.add(single_track);
+
     var single_track_player = new views.Player();
     single_track_player.track = null; // Don't play the track right away
     single_track_player.context = single_track_playlist;
 
     /* Pass the player HTML code to the #single-track-player <div /> */
     var single_track_player_HTML = document.getElementById('single-track-player');
-    console.log(single_track_player_HTML);
     single_track_player_HTML.appendChild(single_track_player.node);
-    
+
+    // Handle multiple tracks player
+    var library_tracks = models.library.tracks;
+
+    var multiple_tracks_playlist = new models.Playlist();
+    for(var i=0;i<20;i++) {
+        var library_track = models.Track.fromURI(library_tracks[i].data.uri);
+        multiple_tracks_playlist.add(library_track);
+    }
+
+    console.log(multiple_tracks_playlist);
+    var multiple_tracks_player = new views.List(multiple_tracks_playlist);
+    multiple_tracks_player.track = null; // Don't play the track right away
+    multiple_tracks_player.context = multiple_tracks_playlist;
+   
+    /* Pass the player HTML code to #multiple-tracks-player */
+    var multiple_tracks_player_HTML = document.getElementById('multiple-tracks-player');
+    multiple_tracks_player_HTML.appendChild(multiple_tracks_player.node);
+
 }
