99148e105a3c819eb87ceb6c8ea55f3e61328e1a
JMPerez <jmperez1985@gmail.com>
2012-12-10 15:42:16 +0100

    Improve 'Getting Started' section, moving section to their own pages

--- js/tutorial.js
+++ js/tutorial.js
@@ -9,14 +9,6 @@ window.onload = function() {
     var models = sp.require('sp://import/scripts/api/models');
     var views = sp.require('sp://import/scripts/api/views');
 
-    // Handle share popup
-    var share_element = document.getElementById('share-popup');
-    var share_content = 'spotify:track:76a6mUM5r7VPexAj37TLjo';
-    share_element.addEventListener('click', displayPopup);
-    function displayPopup() {
-        models.application.showSharePopup(share_element, share_content);
-    }
-
     // Handle tabs
     tabs();
     models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
@@ -30,33 +22,6 @@ window.onload = function() {
         }
         current.style.display = 'block';
     }
-
-
-    // Handle drops
-    var drop_box = document.querySelector('#drop_box');
-
-    drop_box.addEventListener('dragstart', function(e){
-        e.dataTransfer.setData('text/html', this.innerHTML);
-        e.dataTransfer.effectAllowed = 'copy';
-    }, false);
-
-    drop_box.addEventListener('dragenter', function(e){
-        e.dataTransfer.dropEffect = 'copy';
-        this.classList.add('over');
-    }, false);
-
-    drop_box.addEventListener('dragover', function(e){
-        e.dataTransfer.dropEffect = 'copy';
-        return false;
-    }, false);
-
-    drop_box.addEventListener('drop', function(e){
-        var drop = models.Playlist.fromURI(e.dataTransfer.getData('text'));
-        this.classList.remove('over');
-        var success_message = document.createElement('p');
-        success_message.innerHTML = 'Playlist successfully dropped: ' + drop.uri;
-        this.appendChild(success_message);
-    }, false);
 
     // Handle models.player
     var single_track = models.Track.fromURI('spotify:track:0blzOIMnSXUKDsVSHpZtWL');
