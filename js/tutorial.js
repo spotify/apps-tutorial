0fcd8b33ac9f5671734f5833696dd726219ea2f2
JMPerez <jmperez1985@gmail.com>
2012-12-11 09:42:19 +0100

    Splitting Playing pages

--- js/tutorial.js
+++ js/tutorial.js
@@ -17,39 +17,9 @@ window.onload = function() {
         var args = models.application.arguments;
         var current = document.getElementById(args[0]);
         var sections = document.getElementsByClassName('section');
-        for (i=0;i<sections.length;i++){
+        for (var i=0; i<sections.length; i++){
             sections[i].style.display = 'none';
         }
         current.style.display = 'block';
     }
-
-    // Handle models.player
-    var single_track = models.Track.fromURI('spotify:track:0blzOIMnSXUKDsVSHpZtWL');
-    var single_track_playlist = new models.Playlist();
-    single_track_playlist.add(single_track);
-
-    var single_track_player = new views.Player();
-    single_track_player.track = null; // Don't play the track right away
-    single_track_player.context = single_track_playlist;
-
-    /* Pass the player HTML code to the #single-track-player <div /> */
-    var single_track_player_HTML = document.getElementById('single-track-player');
-    single_track_player_HTML.appendChild(single_track_player.node);
-
-    // Handle multiple tracks player
-    var library_tracks = models.library.tracks;
-
-    var multiple_tracks_playlist = new models.Playlist();
-    for(var i=0;i<20;i++) {
-        var library_track = models.Track.fromURI(library_tracks[i].data.uri);
-        multiple_tracks_playlist.add(library_track);
-    }
-
-    var multiple_tracks_player = new views.List(multiple_tracks_playlist);
-    multiple_tracks_player.track = null; // Don't play the track right away
-    multiple_tracks_player.context = multiple_tracks_playlist;
-
-    /* Pass the player HTML code to #multiple-tracks-player */
-    var multiple_tracks_player_HTML = document.getElementById('multiple-tracks-player');
-    multiple_tracks_player_HTML.appendChild(multiple_tracks_player.node);
 };
