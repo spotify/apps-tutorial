4a79f19d5648bb9d7925ce058f4485ec2801a720
JMPerez <jmperez1985@gmail.com>
2012-12-12 10:32:41 +0100

    Fix tabs

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,15 +1,9 @@
-/* Code by Andrew Mager
-*
-*  Spotify green: #74c043
-*
-**/
 "use strict";
 window.onload = function() {
     var sp = getSpotifyApi(1);
     var models = sp.require('sp://import/scripts/api/models');
     var views = sp.require('sp://import/scripts/api/views');
 
-    // Handle tabs
     tabs();
     models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
 
@@ -17,8 +11,10 @@ window.onload = function() {
         var args = models.application.arguments;
         var current = document.getElementById(args[0]);
         var sections = document.getElementsByClassName('section');
-        for (var i=0; i<sections.length; i++){
-            sections[i].style.display = 'none';
+        for (var i=0, l = sections.length; i<l; i++){
+            if (current != sections[i]) {
+                sections[i].style.display = 'none';
+            }
         }
         current.style.display = 'block';
     }
