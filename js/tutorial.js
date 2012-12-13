93916b427c3e580bca724814dc72381d097d8fe6
JMPerez <jmperez1985@gmail.com>
2012-12-13 23:29:55 +0100

    Inject pages using ajax and avoid duplication of code

--- js/tutorial.js
+++ js/tutorial.js
@@ -4,18 +4,60 @@ window.onload = function() {
     var models = sp.require('sp://import/scripts/api/models');
     var views = sp.require('sp://import/scripts/api/views');
 
-    tabs();
-    models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
+    function htmlEscape(str) {
+        return String(str)
+                .replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+    }
 
     function tabs() {
+
         var args = models.application.arguments;
-        var current = document.getElementById(args[0]);
-        var sections = document.getElementsByClassName('section');
-        for (var i=0, l = sections.length; i<l; i++){
-            if (current != sections[i]) {
-                sections[i].style.display = 'none';
-            }
+        if (args.indexOf('index') === -1) {
+            return;
         }
-        current.style.display = 'block';
+
+        // compose file
+        var file = args.length == 1 ? 'index.html' : '/tutorials/' + args.slice(0, args.length-1).join('/') + '.html';
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', file);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4 || xhr.status != 200) return;
+            var wrapper = document.getElementById('wrapper');
+            wrapper.innerHTML = '<ul class="breadcrumb"><li><a href="/index.html">&laquo; Back to main page</a></li></ul>' + xhr.responseText;
+
+            window.scrollTo(0, 0);
+            var htmlSnippets = wrapper.querySelectorAll(".html-snippet");
+            for (i = 0; i < htmlSnippets.length; i++) {
+                container = htmlSnippets[i].getAttribute("data-container");
+                if (container) {
+                    document.getElementById(container).innerHTML = '<pre><code data-language="html">' + htmlEscape(htmlSnippets[i].innerHTML) + '</code></pre>';
+                }
+            }
+
+            // search js snippets
+            var scripts = wrapper.querySelectorAll("script");
+            for (var i = 0; i < scripts.length; i++) {
+                if (scripts[i].getAttribute('type') == 'script/snippet') {
+                    var dataExecute = scripts[i].getAttribute('data-execute');
+                    if (!dataExecute || dataExecute != 'no') {
+                        eval(scripts[i].innerHTML);
+                    }
+                    var container = scripts[i].getAttribute("data-container");
+                    if (container) {
+                        document.getElementById(container).innerHTML = '<pre><code data-language="javascript">' + htmlEscape(scripts[i].innerHTML) + '</code></pre>';
+                    }
+                }
+            }
+
+            // search html snippets
+            Rainbow.color();
+        };
+        xhr.send(null);
     }
+
+    models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
 };
